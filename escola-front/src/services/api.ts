870acb69aebfde1b322633ce5cbd271760ea5708
Rodrigo Temporim <juniortemporim@gmail.com>
2021-11-03 15:04:05 -0300

    Update api.ts

--- escola-front/src/services/api.ts
+++ escola-front/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://unip97-back.herokuapp.com'
+    baseURL: 'https://unip97-back.herokuapp.com/alunos'
 })
 
 export default api;

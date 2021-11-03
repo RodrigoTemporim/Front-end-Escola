8fee4c006336967fe422d4d43468953b65e168c3
Rodrigo Temporim <juniortemporim@gmail.com>
2021-11-03 16:38:04 -0300

    Update server.js

--- escola-front/server.js
+++ escola-front/server.js
@@ -2,11 +2,5 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3102
  
-if(process.env.NODE.ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'front_end', 'build')));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
-    });
-    
-  }
+app.use(express.static('build'))
+app.listen(PORT, () => console.log('Servidor Front em execução!'))

2376a878ec398a7ddf088db26ebaf2064a6cedfe
Rodrigo Temporim <juniortemporim@gmail.com>
2021-11-03 17:17:46 -0300

    Update routes.tsx

--- escola-front/src/routes.tsx
+++ escola-front/src/routes.tsx
@@ -10,8 +10,8 @@ import AlunosLoc from './pages/loc';
 const Routes: React.FC = () => {
     return(
         <Switch>            
-            <Route path='/' exact component={Home} />
-            <Route path='/alunos' exact component={Alunos} />
+            <Route path='/alunos' exact component={Home} />
+            <Route path='/' exact component={Alunos} />
             <Route path='/cadastro_aluno' exact component={AlunosForm} />
             <Route path='/tarefas_cadastro/:id' exact component={AlunosForm} />
             <Route path='/alunos/:id' exact component={AlunosDetail} />
